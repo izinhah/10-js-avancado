9aac93b3eacdf63c4d6da05a3d9c593acef68b91
izinhah <133233107+izinhah@users.noreply.github.com>
2023-07-12 20:56:42 +0000

    isa

--- script.js
+++ script.js
@@ -20,11 +20,41 @@ function inLista(n, l) {
     }
 }
 
-function adicionar() {
+function adicionar() { 
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value)) 
+        let item = documents.createElements('option') 
+        item.text = `valor ${num.value} adicionado` 
+        lista.appendChild(item) 
+        res.innerHTML = ''
+
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus() 
+} 
+
+function finalizar() { 
+    if (valores.length == 0) { 
+        window.alert ('Adicione valores antes de finalizar!') 
+
+    } else {  
+        let tot = valores.length 
+        let maior = valores[0] 
+        let menor = valores [0] 
+        for (let pos in valores) { 
+            if (valores[pos] > maior) 
+            maior = valores[pos] 
+            if (valores[pos] < menor) 
+            menor = valores[pos] 
+
+        } 
+
+        res.innerHTML = '' 
+        res.innerHTML += `<p>Ao todo temos, ${tot} números cadastrados. </p>` 
+        res.innerHTML += `<p>O maior valor informado foi ${maior}. </p>` 
+        res.innerHTML += `<p>O menor valor informado foi ${menor}. </p>`
     }
 
 }
