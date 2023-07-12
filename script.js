84676672acc6f30516fd1fe02517948f392df72b
izinhah <133233107+izinhah@users.noreply.github.com>
2023-07-12 22:35:00 +0000

    isabel

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let res = document.querySelector('div#res')
 let valores = []
 
 function isNumero(n) {
-    if(Number(n) >= 1 && Number(n) <=100) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
         return true 
     } else {
         return false
@@ -12,34 +12,31 @@ function isNumero(n) {
 }
 
 function inLista(n, l) {
-    if(l.indexOf(Number(n)) !=-1 ) {
+    if(l.indexOf(Number(n)) != -1 ) {
         return true 
-
     } else { 
         return false 
     }
 }
 
-function adicionar() { 
+function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        valores.push(Number(num.value)) 
-        let item = documents.createElements('option') 
-        item.text = `Valor ${num.value} adicionado.` 
-        lista.appendChild(item) 
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
         res.innerHTML = ''
-
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
     num.value = ''
-    num.focus() 
-} 
+    num.focus()
+}
 
 function finalizar() { 
-    if (valores.length == 0) { 
+    if (valores.length == 0) {
         window.alert ('Adicione valores antes de finalizar!') 
-
-    } else {  
+    } else {
         let tot = valores.length 
         let maior = valores[0] 
         let menor = valores [0] 
